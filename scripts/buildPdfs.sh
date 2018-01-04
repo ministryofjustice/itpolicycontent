72aa8d56941eb337a21167de52fc2c6998656405
Adrian Warman <adrian.warman@digital.justice.gov.uk>
2018-01-04 17:01:31 +0000

    Start adding list of PDFs to build.

--- scripts/buildPdfs.sh
+++ scripts/buildPdfs.sh
@@ -18,10 +18,37 @@ resultDir="$1"
 mkdir -p $resultDir
 
 array=(
+  .
+  acceptable-use
+  acceptable-use-policy
   access-control
+  clear-screen-and-desk-policy
+  email
+  forensic-principles
+  government-classification-scheme
+  gsi-accreditation
+  ict-security-policy-framework
   ict-security-policy-framework/code-of-connection-standard
   ict-security-policy-framework/offshoring-guide
+  incident-reporting
+  infrastructure-system-accreditation
   laptops
+  locking-and-shutdown
+  lost-laptophardware
+  passwords
+  policies-for-google-apps-administrators
+  policies-for-macbook-administrators
+  policies-for-macbook-users
+  protect-yourself-online
+  removable-media
+  risk-reviews
+  secure-disposal-of-ict-equipment
+  security-clearance
+  source-code-publishing
+  taking-it-equipment-abroad-business-or-personal
+  training-and-education
+  web-browsing
+  web-browsing-security-policy-profiles
 )
 
 BASEURL="https://intranet.justice.gov.uk/guidance/security/it-computer-security"
