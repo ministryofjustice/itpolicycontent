37479615e24daec659f0e3f1ab98a3b24c94cdf0
Adrian Warman <adrian.warman@digital.justice.gov.uk>
2018-09-24 11:26:36 +0100

    Progress the reuse script.

--- scripts/reuser.sh
+++ scripts/reuser.sh
@@ -8,7 +8,7 @@ echo "${progName} [[[source_file start_line end_line] hash_code] target_file ]"
 echo ""
 echo "source_file: Name of incoming content file."
 echo "start_line: First line to include from source_file"
-echo "end_line: Last line to include from source_file. Use 0 to indicate the last line in the file."
+echo "end_line: Last line to include from source_file. Use '$' to indicate the last line in the file."
 echo "hash_code: Expected MD5 hash code for the specified lines from the source_file"
 echo "\tIf absent, report the MD5 hash code for the specified lines from the source_file"
 echo "target_file: Name of file to output to. If the file exists, the results are *appended* to the file. If the file does not exist, it is created."
@@ -32,8 +32,10 @@ else
     # https://unix.stackexchange.com/questions/181937/how-create-a-temporary-file-in-shell-script
     tmpFile=$(mktemp /tmp/reuser-script.XXXXXX)
     trap "rm -f $tmpFile" 0 2 3 15
+    sed ${startLine},${endLine}'!d' ${sourceFile} > ${tmpFile}
+    # echo "Created:"
+    # cat ${tmpFile}
     echo "Proceed."
-    ls /tmp
-    echo "Done."
   fi
 fi
+echo "Done."
