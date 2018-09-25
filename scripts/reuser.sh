8681aa02ca7ce3d1f6350949e0b993fef26c66c8
Adrian Warman <adrian.warman@digital.justice.gov.uk>
2018-09-25 17:34:58 +0100

    Finish first draft of prototype code.

--- scripts/reuser.sh
+++ scripts/reuser.sh
@@ -4,17 +4,23 @@ paramCount=$#
 progName=$0
 
 help () {
-echo "${progName} [[[source_file start_line end_line] hash_code] target_file ]"
+echo "${progName} [[source_file start_line end_line] hash_code ]"
+echo ""
+echo "Return specified segment of text from specified file."
+echo "Generate hash code for specified segment of text from specified file."
 echo ""
 echo "source_file: Name of incoming content file."
 echo "start_line: First line to include from source_file"
 echo "end_line: Last line to include from source_file. Use '$' to indicate the last line in the file."
 echo "hash_code: Expected MD5 hash code for the specified lines from the source_file"
 echo "\tIf absent, report the MD5 hash code for the specified lines from the source_file"
-echo "target_file: Name of file to output to. If the file exists, the results are *appended* to the file. If the file does not exist, it is created."
-echo "\tIf absent, send results to <stdout>."
 echo ""
 echo "If no parameters are supplied, output this help information."
+echo ""
+echo "Error codes returned by the script:"
+echo "0\tNo error."
+echo "-1\tCannot locate input source file."
+echo "-2\tThe specified text segment has a different hash code. This means it has changed."
 }
 
 if [ -e /sbin/md5 ]
@@ -31,11 +37,13 @@ fi
 if [ "${paramCount}" -lt "3" ]
 then
   help
+  exit 0
 else
   sourceFile=${1}
   if [ ! -e "${sourceFile}" ]
   then
     echo "Cannot locate file ${sourceFile}"
+    exit -1
   else
     startLine=${2}
     endLine=${3}
@@ -55,8 +63,19 @@ else
       # Using Linux
       md5Sum=`${md5Exec} ${tmpFile} | awk '{print $1}'`
     fi
-    echo "Hashcode is ${md5Sum}"
-    echo "Proceed."
+    if [ "${paramCount}" -eq "4" ]
+    then
+      if [ "${md5Sum}" = "${4}" ]
+      then
+        cat ${tmpFile}
+      else
+        echo "Re-use content has changed."
+        exit -2
+      fi
+    else
+      # Show the hash code for the content segment
+      echo "${md5Sum}"
+      echo 0
+    fi
   fi
 fi
-echo "Done."
